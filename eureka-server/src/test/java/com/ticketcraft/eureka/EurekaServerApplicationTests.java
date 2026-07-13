8d205436fb095d42986ec111499bf94243cf82e0
arduR-O <omaansh@gmail.com>
2026-07-13 13:29:12 +0530

    feat: implement eureka discovery server with application properties and bootstrap tests

---
+++ eureka-server/src/test/java/com/ticketcraft/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketcraft.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Verification that the Spring Application Context starts cleanly
+    }
+}
